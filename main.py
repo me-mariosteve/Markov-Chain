5efeac98d54a9b86d1cecc7f4307e769d4b02a95
me-mariosteve <93870376+me-mariosteve@users.noreply.github.com>
2021-12-27 12:39:47 +0100

    Added the option gen

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def main(argc, argv):
                 "Usage: main.py OPTION [OPTIONS ...]\n"
                 "Options:\n"
                 "  help        Displays the help about this program.\n"
-                "  calc TEXT   Calculate the porbability for each pattern of two characters to appear in the text TEXT.\n"
+                "  calc TEXT        Calculate the porbability for each pattern of two characters to appear in the text TEXT.\n"
+                "  gen TEXT LENGTH        Generate a random text of length LENGTH by using the probability of each pair of character in the text TEXT."
             )
             return 0
 
@@ -33,6 +34,18 @@ def main(argc, argv):
             else:
                 print("Usage of the option 'calc': main.py calc TEXT")
                 return 1
+        
+        elif option == "gen":
+                if argc != 4:
+                    print("Usage of the option 'gen': main.py gen TEXT LENGTH")
+                    return 1
+                
+                else:
+                    text, length = argv[2], int(argv[3])
+                    probs = markov.calc_probs(text)
+                    out = markov.gen(probs, length)
+                    print(out)
+                    return 0
 
 
 if __name__ == "__main__":
