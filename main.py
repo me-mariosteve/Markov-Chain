866b2dc8b3b3b32fc398d57b50e44c6307d57d56
me-mariosteve <93870376+me-mariosteve@users.noreply.github.com>
2021-12-27 11:16:00 +0100

    Create main.py . I will finish it later.

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+import markov
+
+
+def main(argc, argv):
+    if argc == 1:
+        print("Usage: main.py OPTION [OPTIONS ...}")
+        return 0
+
+
+if __name__ == "__main__":
+    return main(sys.argc, sys.argv)
